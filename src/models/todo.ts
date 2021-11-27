9658db519953f222d406639b33be61c4d429f157
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-11-27 23:03:39 +0900

    namespaceでいいのでは？

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -10,7 +10,7 @@ import {
 import { User } from "src";
 import { Merge } from "type-fest";
 
-export type TodoData = {
+export type Data = {
   title: string;
   completed: boolean;
   createdAt: Timestamp;
@@ -21,32 +21,35 @@ export type TodoData = {
   };
 };
 
+export type IModel = WithIdAndRef<Data>;
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Model extends IModel {}
+// export class Model {
+//   constructor(init: IModel) {
+//     Object.assign(this, init);
+//   }
+// }
+
+export const converter = createConverter<Data>();
+
+export const collectionPath = ({ userId }: { userId: string }) => `users/${userId}/todos`;
+
+export const docPath = ({ userId, todoId }: { userId: string; todoId: string }) =>
+  `users/${userId}/todos/${todoId}`;
+
+export const collectionRef = createTypedCollectionRef(collectionPath, converter);
+
+export const docRef = createTypedDocRef(docPath, converter);
+
 type DefaultDataToReturn = Omit<
-  Merge<TodoData, { createdAt: FieldValue; updatedAt: FieldValue }>,
+  Merge<Data, { createdAt: FieldValue; updatedAt: FieldValue }>,
   "creator"
 >;
 
-export type ITodo = WithIdAndRef<TodoData>;
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Todo extends ITodo {}
-
-export class Todo {
-  static readonly converter = createConverter<TodoData>();
-  static readonly collectionPath = ({ userId }: { userId: string }) => `users/${userId}/todos`;
-  static readonly docPath = ({ userId, todoId }: { userId: string; todoId: string }) =>
-    `users/${userId}/todos/${todoId}`;
-  static readonly collectionRef = createTypedCollectionRef(this.collectionPath, this.converter);
-  static readonly docRef = createTypedDocRef(this.docPath, this.converter);
-
-  static readonly defaultDataTo = (): DefaultDataToReturn => ({
-    title: "",
-    completed: false,
-    createdAt: serverTimestamp(),
-    updatedAt: serverTimestamp(),
-  });
-
-  constructor(init: ITodo) {
-    Object.assign(this, init);
-  }
-}
+export const defaultDataTo = (): DefaultDataToReturn => ({
+  title: "",
+  completed: false,
+  createdAt: serverTimestamp(),
+  updatedAt: serverTimestamp(),
+});
